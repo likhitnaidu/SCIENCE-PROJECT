79e25fdf6b5b7ce9f4cd5bb7385fdc2d77ad2091
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-06-06 20:52:54 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: 'What does heart pump?',
+    answer: 'red oxyginated blood ',
+    options: [
+      'white blood cells',
+      'lymph',
+      'water(H2O)',
+      'red oxyginated blood',
+    ],
+  },
+  {
+    numb: 2,
+    question: 'What does MRI stand for?',
+    answer: 'MAGNETIC RESONANCE IMAGING',
+    options: [
+      'MAGNETIC ROTATING IMAGE',
+      'MAGNETIC RESONANCE DEVICE',
+      'MULTI RATING INVIOCE',
+      'MAGNETIC RESONANCE INDENTIFIER',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'What does PHP stand for?',
+    answer: 'Hypertext Preprocessor',
+    options: [
+      'Hypertext Preprocessor',
+      'Hypertext Programming',
+      'Hypertext Preprogramming',
+      'Hometext Preprocessor',
+    ],
+  },
+  {
+    numb: 4,
+    question: 'What does SQL stand for?',
+    answer: 'Structured Query Language',
+    options: [
+      'Stylish Question Language',
+      'Stylesheet Query Language',
+      'Statement Question Language',
+      'Structured Query Language',
+    ],
+  },
+  {
+    numb: 5,
+    question: 'What does XML stand for?',
+    answer: 'eXtensible Markup Language',
+    options: [
+      'eXtensible Markup Language',
+      'eXecutable Multiple Language',
+      'eXTra Multi-Program Language',
+      'eXamine Multiple Language',
+    ],
+  },
+];
