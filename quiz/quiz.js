f571587e227b107855665f20718fdee69c2bb057
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-06-07 22:33:55 +0530

    Create quiz.js

---
+++ quiz/quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: 'What does heart pump?',
+    answer: 'red oxyginated blood ',
+    options: [
+      'white blood cells',
+      'lymph',
+      'water(H2O)',
+      'red oxyginated blood',
+    ],
+  },
+  {
+    numb: 2,
+    question: 'What does MRI stand for?',
+    answer: 'MAGNETIC RESONANCE IMAGING',
+    options: [
+      'MAGNETIC ROTATING IMAGE',
+      'MAGNETIC RESONANCE DEVICE',
+      'MULTI RATING INVIOCE',
+      'MAGNETIC RESONANCE INDENTIFIER',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'what happens when we inhale?',
+    answer: 'muscles contact',
+    options: [
+      'musles relax',
+      'muscles contact',
+      'lungs shrink',
+      'diaphgm goes out',
+    ],
+  },
+  {
+    numb: 4,
+    question: 'What is the mode of nutrition in fungi??',
+    answer: 'saptrotrophic',
+    options: [
+      'saptrotrophic',
+      'holozoic',
+      'parasatic',
+      'autotrophic',
+    ],
+  },
+  {
+    numb: 5,
+    question: 'What does XML stand for?',
+    answer: 'eXtensible Markup Language',
+    options: [
+      'eXtensible Markup Language',
+      'eXecutable Multiple Language',
+      'eXTra Multi-Program Language',
+      'eXamine Multiple Language',
+    ],
+  },
+];
