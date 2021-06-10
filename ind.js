0394c6af573d0ef17b03a2c82fbb039a9bdb702f
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-06-10 18:23:34 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -233,3 +233,19 @@ function queCounter(index) {
     '</p> Questions</span>';
   bottom_ques_counter.innerHTML = totalQueCounTag; //adding new span tag inside bottom_ques_counter
 }
+ const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: 'dark mode', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
