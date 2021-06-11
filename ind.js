99272e08991d7e00d4f6bc2852e2ba666b9decb7
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-06-11 20:21:20 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -237,10 +237,10 @@ function queCounter(index) {
      
 
  const options = {
-  bottom: '10px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '80px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
