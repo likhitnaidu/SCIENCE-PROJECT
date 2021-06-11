4150427fc31cc3e5841a961b9abcd2c1f125e71a
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-06-11 20:10:48 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -233,65 +233,8 @@ function queCounter(index) {
     '</p> Questions</span>';
   bottom_ques_counter.innerHTML = totalQueCounTag; //adding new span tag inside bottom_ques_counter
 }
-let questions = [
-  {
-    numb: 1,
-    question: 'What does heart pump?',
-    answer: 'red oxyginated blood ',
-    options: [
-      'white blood cells',
-      'lymph',
-      'water(H2O)',
-      'red oxyginated blood',
-    ],
-  },
-  {
-    numb: 2,
-    question: 'What does MRI stand for?',
-    answer: 'MAGNETIC RESONANCE IMAGING',
-    options: [
-      'MAGNETIC ROTATING IMAGE',
-      'MAGNETIC RESONANCE DEVICE',
-      'MULTI RATING INVIOCE',
-      'MAGNETIC RESONANCE INDENTIFIER',
-    ],
-  },
-  {
-    numb: 3,
-    question: 'what happens when we inhale?',
-    answer: 'muscles contact',
-    options: [
-      'musles relax',
-      'muscles contact',
-      'lungs shrink',
-      'diaphgm goes out',
-    ],
-  },
-  {
-    numb: 4,
-    question:'what does the liver excret?',
-    answer:'bile juice',
-    options:[
-      'mango juice'
-      'pepsin'
-      'dillute HCL'
-      'bile juice'
-   ]
-  },
-  
-  {
-    numb: 5,
-    question: 'how long is small intestine?',
-    answer: '22 feet',
-    options: [
-      '100 feet',
-      '22 feet',
-      '23.5 feet',
-      '6 feet',
-    ],
-  },
-];
- 
+
+     
 
  const options = {
   bottom: '10px', // default: '32px'
