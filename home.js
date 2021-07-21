d54b304edf056f5290c568ea6cb89cd984f01415
M.Likhit Naidu <71972560+likhitnaidu@users.noreply.github.com>
2021-07-21 20:31:19 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,19 @@
-var=document.getelementbyclass(".drop").click(function() {
-	var1=document.getelementbyclass(".stem").addClass("rain");
-	
-	setTimeout(function () { 
-		$('.stem').removeClass('rain');
-	}, 1200);  
-});
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
